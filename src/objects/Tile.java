517abb6ffd0fed3f8855c94ba74af95730e73a5a
100-100 <58492711+100-100@users.noreply.github.com>
2020-11-18 21:51:07 -0500

    created skeleton of Tile class

---
+++ src/objects/Tile.java
@@ -0,0 +1,72 @@
+package objects;
+
+public class Tile {
+	private String type;
+	private String item;
+	private boolean moveable;
+	private int orientation;
+	private int x, y;
+	
+	public Tile(String type, String item, boolean moveable, int orientation, int x, int y) {
+		this.type = type;
+		this.item = item;
+		this.moveable = moveable;
+		this.orientation = orientation;
+		this.x = x;
+		this.y = y;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
+	public boolean isMoveable() {
+		return moveable;
+	}
+
+	public void setMoveable(boolean moveable) {
+		this.moveable = moveable;
+	}
+
+	public int getOrientation() {
+		return orientation;
+	}
+
+	public void setOrientation(int orientation) {
+		this.orientation = orientation;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		return "Tile [type=" + type + ", item=" + item + ", moveable=" + moveable + ", orientation=" + orientation
+				+ ", x=" + x + ", y=" + y + "]";
+	}
+}
