144411b7a905faa6180dc9fb01e72a437e610c7b
100-100 <58492711+100-100@users.noreply.github.com>
2020-11-18 22:40:54 -0500

    Created Deck class with built in shuffling

---
+++ src/objects/Deck.java
@@ -0,0 +1,41 @@
+package objects;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Deck {
+	private Stack<Card> deck = new Stack<Card>();
+	
+	public Deck() {
+		Scanner input;
+		
+		try {
+			input = new Scanner(new File("Treasures.txt"));
+			
+			for (int i = 0; i < 24; i++) {
+				deck.add(new Card(input.next()));
+			}
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("ERROR. File not found.");
+		}
+		
+		Collections.shuffle(deck);
+	}
+
+	public Stack<Card> getDeck() {
+		return deck;
+	}
+
+	public void setDeck(Stack<Card> deck) {
+		this.deck = deck;
+	}
+
+	@Override
+	public String toString() {
+		return "Deck [deck=" + deck + "]";
+	}
+}
