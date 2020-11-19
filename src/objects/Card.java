49c0bfe08b84c6501d6b5b29c55567dec822849c
100-100 <58492711+100-100@users.noreply.github.com>
2020-11-18 22:52:36 -0500

    Ensuring that I commited this XD

--- src/objects/Card.java
+++ src/objects/Card.java
@@ -6,18 +6,13 @@ public class Card {
 	public Card(String item) {
 		this.item = item;
 	}
-
+	
 	public String getItem() {
 		return item;
-	}
-
-	public void setItem(String item) {
-		this.item = item;
 	}
 
 	@Override
 	public String toString() {
 		return "Card [item=" + item + "]";
 	}
-	
 }
