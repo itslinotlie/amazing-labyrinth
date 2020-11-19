80d1bbb79e9297b2cba795733fd8af1df9765a86
100-100 <58492711+100-100@users.noreply.github.com>
2020-11-18 22:50:28 -0500

    Created skeleton of Board class

--- src/objects/Board.java
+++ src/objects/Board.java
@@ -1,5 +1,20 @@
 package objects;
 
-public class Board {
+import java.util.Arrays;
 
+public class Board {
+	private Tile[][] tiles = new Tile[7][7];
+
+	public Tile[][] getTiles() {
+		return tiles;
+	}
+
+	public void setTiles(Tile[][] tiles) {
+		this.tiles = tiles;
+	}
+
+	@Override
+	public String toString() {
+		return "Board [tiles=" + Arrays.toString(tiles) + "]";
+	}
 }
