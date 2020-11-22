e72c88a974960c35549aeae4077cbf20308e0574
100-100 <58492711+100-100@users.noreply.github.com>
2020-11-22 17:54:05 -0500

    Completed and tested Board class

--- src/objects/Board.java
+++ src/objects/Board.java
@@ -4,11 +4,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
-public class Board {	
+public class Board {
+	
 	private Tile[][] board = new Tile[9][9];
 	private Tile freeTile;
 	
@@ -16,8 +16,7 @@ public class Board {
 		int orientation;
 		int row = 1;
 		int col = 1;
-		boolean[][] taken = new boolean[9][9];
-		Queue<Tile> tiles = new LinkedList<Tile>();
+		Stack<Tile> tiles = new Stack<Tile>();
 		Tile tile;
 		Scanner input;
 		
@@ -26,29 +25,30 @@ public class Board {
 			input.useDelimiter(",");
 			
 			for (int i = 0; i < 16; i++) { 									// adding non movable tiles to board
-				tile = new Tile(input.next(), input.next(), input.nextBoolean(), input.nextInt(), input.nextInt(), input.nextInt());
+				tile = new Tile(input.next().replaceAll("\n", "").replaceAll("\r", ""), input.next(), input.nextBoolean(), input.nextInt(), input.nextInt(), input.nextInt());
 				board[tile.getX()][tile.getY()] = tile;
-				taken[tile.getX()][tile.getY()] = true;
 			}
 			
-			for (int i = 0; i < 34; i++) { // adding all the movable tiles to a queue so that they can be randomly added to the board
-				tiles.add(new Tile(input.next(), input.next(), input.nextBoolean(), input.nextInt(), input.nextInt(), input.nextInt()));
+			for (int i = 0; i < 34; i++) { 						// adding all the movable tiles to a queue so that they can be randomly added to the board
+				tiles.add(new Tile(input.next().replaceAll("\n", "").replaceAll("\r", ""), input.next(), input.nextBoolean(), input.nextInt(), input.nextInt(), input.nextInt()));
 			}
-			Collections.shuffle(Arrays.asList(tiles));
-			
-			for (int i = 0; i < 34; i++) { // randomly assigning orientation and position of tile, and adding to board
-				while(tiles.size() != 1) {
-					orientation = (int) ((Math.random() * (3 - 0)) + 0);
-					while(!taken[row][col]) {
-						row = (int) ((Math.random() * (7 - 1)) + 1);
-						col = (int) ((Math.random() * (7 - 1)) + 1);
+			Collections.shuffle(tiles);
+
+			while(tiles.size() != 1) { 				// randomly assigning orientation and position to movable tiles, and adding them to board
+				while(true) {
+					orientation = (int) (Math.random() * 4);
+					row = (int) (Math.random() * 7 + 1);
+					col = (int) (Math.random() * 7 + 1);
+					
+					if (board[row][col] == null) {
+						tile = tiles.pop();
+						board[row][col] = new Tile(tile.getType(), tile.getItem(), tile.isMoveable(), orientation, row, col);
+						break;
 					}
-					tile = tiles.poll();
-					board[row][col] = new Tile(tile.getType(), tile.getItem(), tile.isMoveable(), orientation, row, col);
 				}
 			}
-			freeTile = tiles.poll();
-			
+			freeTile = tiles.pop();
+	
 		} catch (FileNotFoundException e) {
 			System.out.println("ERROR. File not found.");
 		}
