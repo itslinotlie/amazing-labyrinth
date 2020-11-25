ce65d89403e6d358e0937402725a0f4837835b2d
100-100 <58492711+100-100@users.noreply.github.com>
2020-11-25 16:32:24 -0500

    added boolean array found, removed playerID

--- src/objects/Player.java
+++ src/objects/Player.java
@@ -3,20 +3,29 @@ package objects;
 import java.util.Arrays;
 
 public class Player {
-	private Card[] hand;
-	private int playerID;
 	private int row, col;
 	private boolean playing;
+	private boolean[] found;
+	private Card[] hand;
 	
-	public Player(int numCards, int playerID, int row, int col, boolean playing) {
+	
+	public Player(int numCards, int row, int col, boolean playing) {
 		hand = new Card[numCards];
+		found = new boolean[numCards];
 		for (int i = 0; i < numCards; i++) {
 			hand[i] = Deck.getCard();
 		}
-		this.playerID = playerID;
 		this.row = row;
 		this.col = col;
 		this.playing = playing;
+	}
+
+	public boolean[] getFound() {
+		return found;
+	}
+
+	public void setFound(boolean[] found) {
+		this.found = found;
 	}
 
 	public Card[] getHand() {
@@ -25,14 +34,6 @@ public class Player {
 
 	public void setHand(Card[] hand) {
 		this.hand = hand;
-	}
-
-	public int getPlayerID() {
-		return playerID;
-	}
-
-	public void setPlayerID(int playerID) {
-		this.playerID = playerID;
 	}
 
 	public int getRow() {
@@ -61,8 +62,7 @@ public class Player {
 
 	@Override
 	public String toString() {
-		return "Player [hand=" + Arrays.toString(hand) + ", playerID=" + playerID + ", row=" + row + ", col=" + col
-				+ ", playing=" + playing + "]";
+		return "Player [row=" + row + ", col=" + col + ", playing=" + playing + ", found=" + Arrays.toString(found)
+				+ ", hand=" + Arrays.toString(hand) + "]";
 	}
-	
 }
