cee74d222a97dece2b2e3d4c227eebd86d7d61eb
Michael <michael.li.web@gmail.com>
2020-11-27 15:01:56 -0500

    Cleanup Player and Deck class

--- src/objects/Deck.java
+++ src/objects/Deck.java
@@ -8,32 +8,25 @@ import java.util.Stack;
 
 public class Deck {
 	private static Stack<Card> deck = new Stack<Card>();
-	
+
 	public Deck() {
 		Scanner input;
-		
 		try {
 			input = new Scanner(new File("res/Treasures.txt"));
-			
 			for (int i = 0; i < 24; i++) {
 				deck.add(new Card(input.next()));
 			}
-			
 		} catch (FileNotFoundException e) {
-			System.out.println("ERROR. File not found.");
+			System.out.println("ERROR. Card file(s) not found.");
 		}
-		
 		Collections.shuffle(deck);
 	}
-
 	public Card getCard() {
-		return(deck.pop());
+		return deck.pop();
 	}
-	
 	public Stack<Card> getDeck() {
 		return deck;
 	}
-
 	@Override
 	public String toString() {
 		return "Deck [deck=" + deck + "]";
