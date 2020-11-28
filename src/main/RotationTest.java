4cc3eadcfae689c97d0c1df5a65d311bc01b852a
nappingrn <dylan.r.trout@gmail.com>
2020-11-27 19:40:00 -0500

    change formatting and use try with resources to avoid memory leak

--- src/main/RotationTest.java
+++ src/main/RotationTest.java
@@ -9,16 +9,23 @@ import java.io.File;
 import java.util.Scanner;
 
 public class RotationTest {
+
+    public TestPanel panel = new TestPanel();
+
     public static void main(String[] args) { //someone deleted this - .-
         RotationTest test = new RotationTest();
-        Scanner in = new Scanner(System.in);
-        int val = in.nextInt();
-        while(val!=0) {
-            test.rotate(val);
-            val = in.nextInt();
+
+        try( Scanner in = new Scanner(System.in)) { // try with resources to avoid memory leak
+
+            int value = in.nextInt();
+
+            while(value!=0) {
+                test.rotate(value);
+                value = in.nextInt();
+            }
         }
     }
-    public TestPanel panel = new TestPanel();
+
     public RotationTest() {
         JFrame frame = new JFrame("Test");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -27,62 +34,95 @@ public class RotationTest {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
+
+
     public void rotate(double ang) {
         panel.rotate(ang);
     }
+
+
     public class TestPanel extends JPanel {
-        BufferedImage img, cpy;
-        String head = new File("").getAbsolutePath(); ///amazing-labyrinth
+       
+        BufferedImage image;
+        BufferedImage imageCopy;
+        String head = new File("").getAbsolutePath(); //amazing-labyrinth
         public double angle = 0;
+
         public TestPanel() {
             try {
-                img = ImageIO.read(new File(head+"/res/github-icon.png"));
-                cpy = rotateImage(img, 0.0);
+                image = ImageIO.read(new File(head+"/res/github-icon.png"));
+                imageCopy = rotateImage(image, 0.0);
             } catch(Exception e) {
                 System.out.println("Something went wrong ):");
-            } repaint();
+            }  repaint();
         }
+
+
         public void rotate(double ang) {
-            for (int i=0;i<ang;i++) {
+            for (int i = 0; i < ang; i++) {
                 try {
                     Thread.sleep(10);
-                    angle+=1;
-                    cpy = rotateImage(img, angle);
+                    angle += 1;
+                    imageCopy = rotateImage(image, angle);
                     repaint();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         }
-        public BufferedImage rotateImage(BufferedImage img, double angle) {
-            double rad = Math.toRadians(angle), sin = Math.abs(Math.sin(rad)), cos = Math.abs(Math.cos(rad));
-            int w = img.getWidth(), h = img.getHeight();
-            //new width and height of the rotated image
-            int nw = (int)Math.floor(w*cos+h*sin), nh = (int)Math.floor(h*cos+w*sin); //implementation is left for the readers
-            BufferedImage rot = new BufferedImage(nw, nh, BufferedImage.TYPE_INT_ARGB);
-            Graphics2D g2d = rot.createGraphics();
-            AffineTransform at = new AffineTransform();
-            at.translate((nw-w)/2, (nh-h)/2);
 
-            int x = w/2, y = h/2; //center of rotation
-            at.rotate(rad, x, y);
-            g2d.setTransform(at);
-            g2d.drawImage(img, 0, 0, this);
+        public BufferedImage rotateImage(BufferedImage image, double angle) {
+
+            double rad = Math.toRadians(angle);
+            double sin = Math.abs(Math.sin(rad));
+            double cos = Math.abs(Math.cos(rad));
+            
+            int new_width = (int) Math.floor ( //width of the rotated image
+                (image.getWidth() * cos) + 
+                (image.getHeight() * sin)
+            ); 
+
+            int new_height = (int) Math.floor ( //height of the rotated image
+                (image.getHeight() * cos) + 
+                (image.getWidth() * sin)
+            ); 
+
+            BufferedImage rot = new BufferedImage(new_width, new_height, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2d = rot.createGraphics();
+            AffineTransform aTransform = new AffineTransform();
+
+            aTransform.translate ( 
+                ((new_width - image.getWidth()) / 2),
+                ((new_height - image.getHeight()) / 2)
+            );
+
+            int xAxis = (image.getWidth() / 2);
+            int yAxis = (image.getHeight() / 2); //center of rotation
+
+            aTransform.rotate(rad, xAxis, yAxis);
+
+            g2d.setTransform(aTransform);
+            g2d.drawImage(image, 0, 0, this);
             g2d.dispose();
+
             return rot; //return the newly rotated BufferedImage
         }
+
+
         @Override
         public Dimension getPreferredSize() {
-            return img==null? new Dimension(200, 200):new Dimension(img.getWidth(), img.getHeight());
+            return image == null ? new Dimension(200, 200):new Dimension(image.getWidth(), image.getHeight());
         }
+
+
         @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            if(cpy!=null) {
-                Graphics2D g2d = (Graphics2D) g.create();
-                int x = (getWidth()-cpy.getWidth())/2;
-                int y = (getHeight()-cpy.getHeight())/2;
-                g2d.drawImage(cpy, x, y, this);
+        protected void paintComponent(Graphics graphic) {
+            super.paintComponent(graphic);
+            if(imageCopy != null) {
+                Graphics2D g2d = (Graphics2D) graphic.create();
+                int x = (getWidth() - imageCopy.getWidth())/2;
+                int y = (getHeight() - imageCopy.getHeight())/2;
+                g2d.drawImage(imageCopy, x, y, this);
                 g2d.dispose();
             }
         }
