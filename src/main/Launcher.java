bd66ff0649022c2ec63d2691f5ee18f2e7998fda
100-100 <58492711+100-100@users.noreply.github.com>
2020-11-28 19:41:19 -0500

    Started on main method comments

--- src/main/Launcher.java
+++ src/main/Launcher.java
@@ -1,3 +1,39 @@
+/* Names:
+ * Eric Lin(33%), Michael Li(33%), Zain Muhammad(33%)
+ * 
+ * Date:
+ * November 29, 2020
+ * 
+ * Course Code:
+ * ICS4U1-51, Mr. Fernandes
+ * 
+ * Title:
+ * Amazing Labyrinth (project title)
+ * The Amazing Labyrinth (product name)
+ * 
+ * Description:
+ * Our project took the ever popular Amazing Labyrinth board game and recreated it to be played on 
+ * the computer with some slight touches and changes.
+ * 
+ * Features:
+ *  	- potential pathways a player can take are highlighted
+ *      - Game can be saved and reloaded 
+ *      - player movement is animated and not directly placed
+ *      - smooth rotation of tiles
+ *      - 
+ * 
+ * Major Skills:
+ * 		- knowledge of graphical user interfaces 
+ *  	- basic graph theory/graph traversal algorithms 
+ *  	- Object oriented programming practices (packages, classes)
+ *      - Polymorphism and inheritance 
+ *      - file access and creation
+ *      - 
+ * 
+ * Areas of Concern: 
+ * 		- 
+ * 
+ */
 package main;
 import guiClasses.StartScreenGUI;
 
