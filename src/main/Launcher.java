b3df11edce43c5b19764730662c925b99faef938
100-100 <58492711+100-100@users.noreply.github.com>
2020-11-29 21:28:51 -0500

    Finalized the main method comments

--- src/main/Launcher.java
+++ src/main/Launcher.java
@@ -1,44 +1,3 @@
-/* Names:
- * Eric Lin(33%), Michael Li(33%), Zain Muhammad(33%)
- * Eric Lin - GUI
- * Michael Li - logic & management
- * Zain Muhammad - classes/objects
- * 
- * Date:
- * November 29, 2020
- * 
- * Course Code:
- * ICS4U1-51, Mr. Fernandes
- * 
- * Title:
- * Amazing Labyrinth (project title)
- * The Amazing Labyrinth (product name)
- * 
- * Description:
- * Our project took the ever popular Amazing Labyrinth board game and recreated it to be played on 
- * the computer with some slight touches and changes.
- * 
- * Features:
- *  	- potential pathways a player can take are highlighted
- *      - Game can be saved and reloaded 
- *      - player movement is animated and not directly placed
- *      - smooth rotation of tiles
- *      - 
- * 
- * Major Skills:
- * 		- knowledge of graphical user interfaces 
- *  	- basic graph theory/graph traversal algorithms 
- *  	- Object oriented programming practices (packages, classes)
- *      - Polymorphism and inheritance 
- *      - file access and creation
- *      - 
- * 
- * Areas of Concern: 
- * 		- the size of our game is roughly 10mb, and should be able to run on almost all systems thanks
- *        to the low system requirements to run the game.
- *      - 
- * 
- */
 package main;
 
 import guiClasses.StartScreenGUI;
@@ -75,7 +34,7 @@ import guiClasses.StartScreenGUI;
  *   -The use of Git as a version control system
  *
  * Areas of Concern:
- *   -Some tile images are not aligned with others, making the board seem “off”
+ *   -Some tile images are not aligned with others, making the board seem off
  *   -Players cannot be fully customizable (i.e. player name, icon etc.)
  */
 public class Launcher {
